fe951ace8df3d84972577dff1ab878be2dabb334
Spencer Gibb <spencer@gibb.us>
2015-03-31 14:08:00 -0600

    set spring.application.name if PROCESS_GUID env var is set.
    PROCESS_GUID is set by lattice

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ Preview of Spring Cloud Lattice implementation
 1. [Install lattice](http://lattice.cf/docs/getting-started.html)
 2. Follow the getting started guide, including scaling `lattice-app` to 3
 4. run `mvn  --settings .settings.xml package`
-5. run `java -jar spring-cloud-lattice-sample/target/spring-cloud-lattice-sample-1.0.0.BUILD-SNAPSHOT.jar`
+5. run `PROCESS_GUID=spring-cloud-lattice-sample java -jar spring-cloud-lattice-sample/target/spring-cloud-lattice-sample-1.0.0.BUILD-SNAPSHOT.jar`
 6. visit [http://localhost:8080?service=lattice-app](http://localhost:8080?service=lattice-app) verify that the 3 services rotate through as you refresh
 
 ### Running the sample ON LATTICE
--- spring-cloud-lattice-discovery/src/main/java/org/springframework/cloud/lattice/discovery/LatticeDiscoveryClientConfiguration.java
+++ spring-cloud-lattice-discovery/src/main/java/org/springframework/cloud/lattice/discovery/LatticeDiscoveryClientConfiguration.java
@@ -1,8 +1,16 @@
 package org.springframework.cloud.lattice.discovery;
 
+import java.util.Collections;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.SmartInitializingSingleton;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.MapPropertySource;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import io.pivotal.receptor.client.ReceptorClient;
@@ -13,7 +21,10 @@ import io.pivotal.receptor.client.ReceptorClient;
 @Configuration
 @EnableScheduling
 @EnableConfigurationProperties
-public class LatticeDiscoveryClientConfiguration {
+public class LatticeDiscoveryClientConfiguration implements ApplicationContextAware,
+		SmartInitializingSingleton {
+
+	private ApplicationContext applicationContext;
 
 	@Bean
 	public ReceptorClient receptorClient() {
@@ -28,5 +39,27 @@ public class LatticeDiscoveryClientConfiguration {
 	@Bean
 	public LatticeDiscoveryProperties latticeDiscoveryProperties() {
 		return new LatticeDiscoveryProperties();
+	}
+
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext)
+			throws BeansException {
+		this.applicationContext = applicationContext;
+	}
+
+	@Override
+	public void afterSingletonsInstantiated() {
+		if (this.applicationContext.getEnvironment() instanceof ConfigurableEnvironment) {
+			final ConfigurableEnvironment environment = (ConfigurableEnvironment) this.applicationContext
+					.getEnvironment();
+			Object processGuid = System.getenv("PROCESS_GUID");
+			if (processGuid != null) {
+				MapPropertySource propertySource = new MapPropertySource(
+						"Spring Cloud Lattice Environment", Collections.singletonMap(
+								"spring.application.name", processGuid));
+				environment.getPropertySources().addBefore("systemProperties",
+						propertySource);
+			}
+		}
 	}
 }
