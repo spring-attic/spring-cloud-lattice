5b4d358dad8db5e1d9fc842e9f6e73212ae8c452
Spencer Gibb <spencer@gibb.us>
2015-04-16 14:12:23 -0600

    removed /me / change for guide

--- spring-cloud-lattice-sample/src/main/java/org/springframework/cloud/lattice/sample/SampleLatticeApplication.java
+++ spring-cloud-lattice-sample/src/main/java/org/springframework/cloud/lattice/sample/SampleLatticeApplication.java
@@ -57,12 +57,12 @@ public class SampleLatticeApplication {
 	@Autowired(required = false)
 	RelaxedPropertyResolver resolver;
 
-	@RequestMapping("/")
+	@RequestMapping("/me")
 	public ServiceInstance me() {
 		return discoveryClient.getLocalServiceInstance();
 	}
 
-	@RequestMapping("/lb")
+	@RequestMapping("/")
 	public ServiceInstance lb(
 			@RequestParam(value = "service", defaultValue = CLIENT_NAME) String serviceId) {
 		return loadBalancer.choose(serviceId);
