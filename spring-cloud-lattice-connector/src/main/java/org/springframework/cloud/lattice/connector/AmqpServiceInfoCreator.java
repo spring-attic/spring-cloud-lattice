7f6a697f1bf2f373026f38d2f3ea40830b1f4570
Spencer Gibb <spencer@gibb.us>
2015-04-29 13:39:41 -0600

    work around default vhost bug in AmqpServiceInfo

--- spring-cloud-lattice-connector/src/main/java/org/springframework/cloud/lattice/connector/AmqpServiceInfoCreator.java
+++ spring-cloud-lattice-connector/src/main/java/org/springframework/cloud/lattice/connector/AmqpServiceInfoCreator.java
@@ -19,6 +19,7 @@ package org.springframework.cloud.lattice.connector;
 import org.springframework.cloud.service.common.AmqpServiceInfo;
 
 import io.pivotal.receptor.commands.ActualLRPResponse;
+import org.springframework.cloud.util.UriInfo;
 
 /**
  * @author Spencer Gibb
@@ -36,8 +37,42 @@ public class AmqpServiceInfoCreator extends LatticeServiceInfoCreator<AmqpServic
 		int port = actual.getPorts()[0].getHostPort();
 		String username = "guest";
 		String password = "guest";
-		String virtualHost = "";
-		return new AmqpServiceInfo(actual.getInstanceGuid(), address, port, username, password, virtualHost);
+		String virtualHost = null;
+		return new LatticeAmqpServiceInfo(actual.getInstanceGuid(), address, port, username, password, virtualHost);
+	}
+
+
+	private static class LatticeAmqpServiceInfo extends AmqpServiceInfo {
+		public LatticeAmqpServiceInfo(String id, String host, int port, String username, String password, String virtualHost) {
+			super(id, host, port, username, password, virtualHost);
+		}
+
+		@Override
+		protected UriInfo validateAndCleanUriInfo(UriInfo uriInfo) {
+			if (uriInfo.getScheme() == null) {
+				throw new IllegalArgumentException("Missing scheme in amqp URI: " + uriInfo);
+			}
+
+			if (uriInfo.getHost() == null) {
+				throw new IllegalArgumentException("Missing authority in amqp URI: " + uriInfo);
+			}
+
+			if (uriInfo.getUserName() == null || uriInfo.getPassword() == null) {
+				throw new IllegalArgumentException("Missing userinfo in amqp URI: " + uriInfo);
+			}
+
+			String path = uriInfo.getPath();
+			if (path == null) {
+				//NO-OP, this is the default vhost
+			} else {
+				// Check that the path only has a single segment.  As we have an authority component
+				// in the URI, paths always begin with a slash.
+				if (path.indexOf('/') != -1) {
+					throw new IllegalArgumentException("Multiple segments in path of amqp URI: " + uriInfo);
+				}
+			}
+			return uriInfo;
+		}
 	}
 
 }
