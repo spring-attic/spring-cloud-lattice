a4b0de20fcedeb9da14ef119a562021e06bd9a96
Dave Syer <dsyer@pivotal.io>
2015-09-09 10:41:31 +0100

    Add convenience annotation

---
+++ spring-cloud-lattice-discovery/src/main/java/org/springframework/cloud/lattice/discovery/EnableLatticeDiscoveryClient.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2013-2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.cloud.lattice.discovery;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * Convenience annotation for clients to enable Lattice discovery configuration
+ * (specifically). Use this (optionally) in case you want discovery and know for sure that
+ * it is Lattice you want. All it does is turn on discovery and let the autoconfiguration
+ * find the eureka classes if they are available (i.e. you need receptor on the classpath
+ * as well).
+ *
+ * @author Dave Syer
+ * @author Spencer Gibb
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Inherited
+@EnableDiscoveryClient
+public @interface EnableLatticeDiscoveryClient {
+
+}
